da309f62cd5f99a07ca7fa07352105bd17a4cfc1
AncNeg <Anca.Negoita@softwire.com>
2024-07-03 17:50:35 +0300

    Base code Gilded Rose

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -7,33 +7,56 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
+            match item.name:
+                case "Aged Brie":
                     if item.quality < 50:
-                        item.quality = item.quality + 1
+                        item.quality += 1
+                case "Sulfuras, Hand of Ragnaros":
+                    continue
+                case item.name if "Backstage passes" in item.name:
+                    if item.sell_in > 0:
+                        item.quality += 3
+                    else:
+                        item.quality += 2
+                case item.name if "Conjured" in item.name:
+                    if item.sell_in > 0:
+                        item.quality = max(0, item.sell_in - 2)
+                    else:
+                        item.quality = max(0, item.sell_in - 4)
+                case _:
+                    if item.sell_in > 0:
+                        item.quality = max(0, item.sell_in - 1)
+                    else:
+                        item.quality = max(0, item.sell_in - 2)
+            item.sell_in -= 1
+
+            # if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+            #     if item.quality > 0:
+            #         if item.name != "Sulfuras, Hand of Ragnaros":
+            #             item.quality = item.quality - 1
+            # else:
+            #     if item.quality < 50:
+            #         item.quality = item.quality + 1
+            #         if item.name == "Backstage passes to a TAFKAL80ETC concert":
+            #             if item.sell_in < 11:
+            #                 if item.quality < 50:
+            #                     item.quality = item.quality + 1
+            #             if item.sell_in < 6:
+            #                 if item.quality < 50:
+            #                     item.quality = item.quality + 1
+            # if item.name != "Sulfuras, Hand of Ragnaros":
+            #     item.sell_in = item.sell_in - 1
+            # if item.sell_in < 0:
+            #     if item.name != "Aged Brie":
+            #         if item.name != "Backstage passes to a TAFKAL80ETC concert":
+            #             if item.quality > 0:
+            #                 if item.name != "Sulfuras, Hand of Ragnaros":
+            #                     item.quality = item.quality - 1
+            #         else:
+            #             item.quality = item.quality - item.quality
+            #     else:
+            #         if item.quality < 50:
+            #             item.quality = item.quality + 1
 
 
 class Item:
